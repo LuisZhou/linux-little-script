a7d56a6f9365fba2e995170f56d9bde5641c8a77
luiszhou <luis.zhou.zw@gmail.com>
2018-02-01 18:53:40 +0800

    update

--- convert-encoding-to-utf8.sh
+++ convert-encoding-to-utf8.sh
@@ -9,15 +9,15 @@
 
 # Example output of chardet: 'box.csv: GB2312 with confidence 0.360492711041'
 CHARSET="$(chardet "$1" | awk -F ":" '{print $2}'  | awk -F "with" '{print $1}')"
-mkdir -p ./convert
 
 if [ "$CHARSET" != utf-8 ]; then
+	mkdir -p ./convert
 	iconv -f "$CHARSET" -t utf8 "$1" -o ./convert/$1
 fi
 
-dos2unix ./convert/$1  2> /dev/null
+#dos2unix ./convert/$1  2> /dev/null
 
-sed -i -e 1d ./convert/$1
-sed -i -e 2d ./convert/$1
+#sed -i -e 1d ./convert/$1
+#sed -i -e 2d ./convert/$1
 
 echo "convert done!"
